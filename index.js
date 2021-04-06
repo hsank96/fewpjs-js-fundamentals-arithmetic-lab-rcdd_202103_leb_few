bf60210801125424931ec82feca4f062e886a763
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-06 17:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 // Write your code here
 
-let num1=31;
-let num2=2;
-let multiply=num1*num2;
+
+let multiply=31*2;
 console.log(multiply)
+
+let random = Math.random();
+let mod = 29%5
+
+let max = Math.max(20,18,19)
